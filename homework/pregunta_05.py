6f109c0cd42366184865113f9f3306aa548141b3
Juan José Martinez Grisales <josemargri3@gmail.com>
2025-06-19 16:01:14 -0500

    pregunta '05

--- homework/pregunta_05.py
+++ homework/pregunta_05.py
@@ -15,3 +15,20 @@ def pregunta_05():
     [('A', 9, 2), ('B', 9, 1), ('C', 9, 0), ('D', 8, 3), ('E', 9, 1)]
 
     """
+
+    with open("files/input/data.csv", "r", encoding="utf-8") as file:
+        lines = file.readlines()
+
+    result = {}
+    for line in lines:
+        columns = line.strip().split("\t")
+        letter = columns[0]
+        value = int(columns[1])
+        if letter not in result:
+            result[letter] = [value, value]  # [max, min]
+        else:
+            result[letter][0] = max(result[letter][0], value)
+            result[letter][1] = min(result[letter][1], value)
+
+    sorted_result = sorted((k, v[0], v[1]) for k, v in result.items())
+    return sorted_result
