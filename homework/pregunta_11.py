d4a2b95912c6bc447507ee0ac5f1286c9af8573f
Juan José Martinez Grisales <josemargri3@gmail.com>
2025-06-19 16:25:33 -0500

    pregunta 11

--- homework/pregunta_11.py
+++ homework/pregunta_11.py
@@ -8,11 +8,26 @@ utilizar pandas, numpy o scipy.
 
 def pregunta_11():
     """
-    Retorne un diccionario que contengan la suma de la columna 2 para cada
-    letra de la columna 4, ordenadas alfabeticamente.
+    Retorne un diccionario que contenga la suma de la columna 2 para cada
+    letra de la columna 4, ordenadas alfabéticamente.
 
     Rta/
     {'a': 122, 'b': 49, 'c': 91, 'd': 73, 'e': 86, 'f': 134, 'g': 35}
-
-
     """
+    with open("files/input/data.csv", "r", encoding="utf-8") as file:
+        lines = file.readlines()
+
+    result = {}
+
+    for line in lines:
+        columns = line.strip().split("\t")
+        if len(columns) > 3:
+            valor = int(columns[1])
+            letras = columns[3].split(",")  # separa letras individuales
+
+            for letra in letras:
+                if letra not in result:
+                    result[letra] = 0
+                result[letra] += valor
+
+    return dict(sorted(result.items()))
